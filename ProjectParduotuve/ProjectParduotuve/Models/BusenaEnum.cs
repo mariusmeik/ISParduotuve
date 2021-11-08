28aab80192c99eb962548a22ae69faf28b7eba1e
mariusmeik <61392793+mariusmeik@users.noreply.github.com>
2021-11-08 13:01:45 +0200

    .

---
+++ ProjectParduotuve/ProjectParduotuve/Models/BusenaEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectParduotuve.Models
+{
+    public class BusenaEnum
+    {
+        public int id { get; set; }
+        public string busena { get; set; }
+
+        public BusenaEnum(int id)
+        {
+            this.id = id;
+
+        }
+    }
+}
--- ProjectParduotuve/ProjectParduotuve/Models/Product.cs
+++ ProjectParduotuve/ProjectParduotuve/Models/Product.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel;
+
 namespace ProjectParduotuve.Models
 {
     public class Product
     {
+        public Product() { }
+        [DisplayName("pagaminimoData")]
         public DateTime pagaminimoData { get; set; }
+        [DisplayName("pardavimoData")]
         public DateTime pardavimoData { get; set; }
+        [DisplayName("galiojimoData")]
         public DateTime galiojimoData { get; set; }
+        [DisplayName("kiekis")]
         public double kiekis { get; set; }
 
-
-  
     }
 }
