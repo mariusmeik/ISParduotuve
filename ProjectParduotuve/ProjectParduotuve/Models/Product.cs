891f63fc5aefd5e0903188a634908893c8dda67b
mariusmeik <61392793+mariusmeik@users.noreply.github.com>
2021-11-08 12:44:50 +0200

    .

---
+++ ProjectParduotuve/ProjectParduotuve/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectParduotuve.Models
+{
+    public class Product
+    {
+        public DateTime pagaminimoData { get; set; }
+        public DateTime pardavimoData { get; set; }
+        public DateTime galiojimoData { get; set; }
+        public double kiekis { get; set; }
+
+
+  
+    }
+}
