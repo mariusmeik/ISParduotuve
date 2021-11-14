b4cc9a31cc56e5aacba3ab6221efbdc8c0164fb9
mariusmeik <61392793+mariusmeik@users.noreply.github.com>
2021-11-14 20:06:45 +0200

    added classes

---
+++ ProjectParduotuve/ProjectParduotuve/Models/MenesioProognoze.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace ProjectParduotuve.Models
+{
+    public class MenesioProognoze
+    {
+        public MenesioProognoze() { }
+
+        [DisplayName("metai")]
+        public int metai { get; set; }
+        [DisplayName("menuo")]
+        public int menuo { get; set; }
+        [DisplayName("reiksmingas")]
+        public bool reiksmingas { get; set; }
+      
+    }
+}
---
+++ ProjectParduotuve/ProjectParduotuve/Models/ProognozuojamaPreke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace ProjectParduotuve.Models
+{
+    public class ProognozuojamaPreke
+    {
+        public ProognozuojamaPreke() { }
+
+        [DisplayName("ReikalingasKiekis")]
+        public double reikalingasKiekis { get; set; }
+        [DisplayName("pirkimoProognoze")]
+        public double pirkimoProognoze { get; set; }
+
+    }
+}
