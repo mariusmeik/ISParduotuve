3098993137abe1151de89f2a6abe5949f19ffbcc
paulius0706 <paulius.sinkunas@ktu.edu>
2021-12-06 13:52:59 +0200

    awds
    adf

--- ProjectParduotuve/ProjectParduotuve/Controllers/HomeController.cs
+++ ProjectParduotuve/ProjectParduotuve/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjectParduotuve.Models;
 
 namespace ProjectParduotuve.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        // GET: Product/Edit/5
+        //atidarius puslapi
         public ActionResult Index()
         {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
             return View();
         }
     }
